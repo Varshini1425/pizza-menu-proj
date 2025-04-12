8db1a544c06edc4978a595b08a2902ef37dfabcb
varshini <varshinisugavanesh@gmail.com>
2025-04-12 18:27:54 +0530

    checked react app is running

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  /*  strict mode only runs in development mode. 
+      It does not render twice in production.
+      It helps catch bugs early before they cause serious problems.
+  */
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
